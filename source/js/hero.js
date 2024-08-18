80b43bf8455af444aed94de512694c84c5d7aaaa
Evgeny <49340526+rychagoff@users.noreply.github.com>
2024-08-18 16:25:26 +0400

    Добавил tabindex, прописал адаптив для планшетов

--- source/js/hero.js
+++ source/js/hero.js
@@ -2,13 +2,40 @@ import Swiper from 'swiper';
 import { Pagination } from 'swiper/modules';
 
 const heroSwiper = new Swiper('.hero-swiper', {
+  init: false,
   loop: true,
   modules: [ Pagination ],
   pagination: {
-    bulletClass: 'swiper-pagination-bullet hero-swiper__bullet',
     el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+    // bulletElement: 'button type="button"',
+    bulletClass: 'swiper-pagination-bullet hero-swiper__bullet',
+    renderBullet: function (index, className) {
+      // className = 'hero-swiper__bullet';
+      return '<span class="' + className + '" role="button"></span>';
+    },
   },
-  clickable: true,
+  keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  grabCursor: true,
 });
 
+heroSwiper.on('init', () => {
+  const hero = document.querySelector('.hero-swiper');
+  const slides = hero.querySelectorAll('.swiper-slide');
 
+  console.log(hero);
+  console.log(slides);
+
+  slides.forEach((slide) => {
+    if (!slide.classList.contains('swiper-slide-active')) {
+      const slideLink = slide.querySelector('.hero-swiper__link');
+      slideLink.setAttribute('tabindex', -1);
+    }
+  })
+});
+
+heroSwiper.init();
